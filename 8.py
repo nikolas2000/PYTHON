a9c48fa1eed24d8242164566d0465ad47b1060e5
nikolas2000 <47464669+nikolas2000@users.noreply.github.com>
2019-02-17 18:58:23 +0200

    Add files via upload

---
+++ 8.py
@@ -0,0 +1,6 @@
+import twitterk
+api = twitter.Api(consumer_key='QnPGCpS7ZUCKqNSFiaTQjAncb ',
+consumer_secret='rGq0Rmzr6rCgLSrJC88CO2czcR2qKQTV97vtfzEVOSmvKYtcJc',
+access_token_key='1095384478229454848-9ueqhK5FrYa2nCidZCaGheTv8WVFn7',
+access_token_secret='KqwRXcKGBbM60k0BpISDwfBC43yesS2HQeZXh0vb5FNIJ')
+print(api.VerifyCredentials())
